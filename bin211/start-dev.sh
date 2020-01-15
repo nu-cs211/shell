e72b05e7ea1c2516639cebad175c16423ff8e5d2
Jesse A. Tov <jesse.tov@gmail.com>
2020-01-15 13:29:29 -0600

    handles no scl case

--- bin211/start-dev.sh
+++ bin211/start-dev.sh
@@ -3,4 +3,9 @@
 test -f $HOME/.zshrc ||
     echo "# Created automatically for CS 211" > $HOME/.zshrc
 
-DEV211=1 RPS1=$RPS211 exec scl enable devtoolset-8 zsh
+if SCL=$(which scl 2>/dev/null); then
+    DEV211=1 RPS1=$RPS211 exec "$SCL" enable devtoolset-8 zsh
+else
+    DEV211=1 exec zsh
+fi
+
