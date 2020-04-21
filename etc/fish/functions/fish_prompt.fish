75813680c43aed54dc2d5e8b6b6f6bb4aa531265
Jesse A. Tov <jesse.tov@gmail.com>
2020-04-21 02:22:37 -0500

    For older fish version on ubunto eoan.

--- etc/fish/functions/fish_prompt.fish
+++ etc/fish/functions/fish_prompt.fish
@@ -1,3 +1,23 @@
+# From /usr/local/Cellar/fish/3.1.0_1/share/fish/functions/__fish_print_pipestatus.fish
+function __fish_print_pipestatus_VENDORED --description 'Print pipestatus for prompt'
+    set -l left_brace $argv[1]
+    set -l right_brace $argv[2]
+    set -l separator $argv[3]
+    set -l brace_sep_color $argv[4]
+    set -l status_color $argv[5]
+    set -e argv[1 2 3 4 5]
+
+    # only output status codes if some process in the pipe failed
+    # SIGPIPE (141 = 128 + 13) is usually not a failure, see #6375.
+    if string match -qvr '^(0|141)$' $argv
+        set -l sep (set_color normal){$brace_sep_color}{$separator}(set_color normal){$status_color}
+        set -l last_pipestatus_string (string join "$sep" (__fish_pipestatus_with_signal $argv))
+        printf "%s%s%s%s%s%s%s%s%s%s" (set_color normal )$brace_sep_color $left_brace \
+            (set_color normal) $status_color $last_pipestatus_string (set_color normal) \
+            $brace_sep_color $right_brace (set_color normal)
+    end
+end
+
 function fish_prompt --description 'Write out the prompt'
     set -l last_pipestatus $pipestatus
     set -l normal (set_color normal)
@@ -54,7 +74,7 @@ function fish_prompt --description 'Write out the prompt'
 
     # Write pipestatus
     set -l prompt_status (
-        __fish_print_pipestatus " [" "]" "|" \
+        __fish_print_pipestatus_VENDORED " [" "]" "|" \
             (set_color $fish_color_status) \
             (set_color --bold $fish_color_status) \
             $last_pipestatus)
